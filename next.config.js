c8ed6e44e4432859b1aa93040b084f53f928e3bf
Aleksandr <alex.avramenko2004@gmail.com>
2024-01-13 12:24:18 -0500

    Project initialization

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
+    },
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '**',
+          }
+        ],
+      },
+}
 
 module.exports = nextConfig
---
+++ src/pages/_app.tsx
@@ -0,0 +1,28 @@
+// These styles apply to every route in the application
+import '@/styles/globals.css'
+import RootLayout from '../components/layout'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { NextUIProvider } from '@nextui-org/react'
+import NextNProgress from 'nextjs-progressbar'
+ 
+export default function App({ Component, pageProps }: AppProps) {  
+  return (
+    <>
+        <SpeedInsights />
+        <Head>
+            <link rel="shortcut icon" href="/static/favicon.ico" type="image/x-icon" />
+            <title>ArMax Renovation LLC</title>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <NextUIProvider>
+            <RootLayout>
+                <NextNProgress color="#467529" />
+                <Component {...pageProps} />
+            </RootLayout>
+        </NextUIProvider>
+    </>
+  )
+}
