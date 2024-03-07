1949e7be2c1a4cc69600bdbfb3c9ea99a95a0dc6
Aleksandr <alex.avramenko2004@gmail.com>
2024-03-07 06:25:21 -0500

    Initial commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,9 +7,9 @@ const formInputs = [
         type: "text",
         id: "name",
         name: "client_name",
-        placeholder: "First and last name",
+        placeholder: "Ім'я та прізвище",
         required: true,
-        label: `Your name`
+        label: `Ваше ім'я`
     },
     {
         type: "email",
@@ -17,7 +17,7 @@ const formInputs = [
         name: "client_email",
         placeholder: "name@example.com",
         required: true,
-        label: "Your email"
+        label: "Ваша електронна пошта"
     },
     {
         type: "tel",
@@ -25,7 +25,7 @@ const formInputs = [
         name: "client_phone",
         placeholder: "123 456 7890",
         required: true,
-        label: "Your phone number"
+        label: "Ваш номер телефону"
     }
 ]
 
@@ -54,21 +54,21 @@ const Form = () => {
     return (
         <>
             <div className="py-5 lg:py-8 px-4 mx-auto max-w-screen-lg">
-                <h2 className="mb-4 text-3xl tracking-tight font-semibold text-center text-heading md:text-5xl">Contact Us</h2>
+                <h2 className="mb-4 text-3xl tracking-tight font-semibold text-center text-heading md:text-5xl">Зв'язатись з нами</h2>
                 <form ref={form} onSubmit={sendEmail} className="space-y-8 text-center">
                     <div>
-                        <label htmlFor="name" className="block mb-2 text-lg font-medium text-gray-900 text-left lg:text-xl">Your name</label>
+                        <label htmlFor="name" className="block mb-2 text-lg font-medium text-gray-900 text-left lg:text-xl">Ваше ім'я</label>
                         <input    
                             type='text'
                             id='name'
                             name="client_name"
                             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 block w-full p-2.5 text-md rounded-lg"
-                            placeholder="First and lastname"
+                            placeholder="Ім'я та прізвище"
                             required
                         />
                     </div>
                     <div>
-                        <label htmlFor="email" className="block mb-2 text-lg font-medium text-gray-900 text-left lg:text-xl">Your email</label>
+                        <label htmlFor="email" className="block mb-2 text-lg font-medium text-gray-900 text-left lg:text-xl">Ваша електронна пошта</label>
                         <input    
                             type='email'
                             id='email'
@@ -79,27 +79,27 @@ const Form = () => {
                         />
                     </div>
                     <div>
-                        <label htmlFor="phone" className="block mb-2 text-lg font-medium text-gray-900 text-left lg:text-xl">Your email</label>
+                        <label htmlFor="phone" className="block mb-2 text-lg font-medium text-gray-900 text-left lg:text-xl">Ваш номер телефону</label>
                         <input    
                             type='tel'
                             id='phone'
                             name="client_phone"
                             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 block w-full p-2.5 text-md rounded-lg"
-                            placeholder="+1 234 567 8901"
+                            placeholder="+38 234 567 8901"
                             required
                         />
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="message" className="block mb-2 text-lg font-medium text-gray-900 text-left lg:text-xl">Your message</label>
+                        <label htmlFor="message" className="block mb-2 text-lg font-medium text-gray-900 text-left lg:text-xl">Ваше повідомлення</label>
                         <textarea
                             id="message"
                             name="client_message"
                             rows={6}
                             className="block p-2.5 w-full text-md text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300"
-                            placeholder="Let us know how we can help you..."
+                            placeholder="Як ми можемо вам допомогти..."
                         />
                     </div>
-                    <input type="submit" value="Message us" className="text-white px-10 py-2 bg-red-700 font-medium rounded-lg text-xl text-center hover:cursor-pointer"/>
+                    <input type="submit" value="Відправити повідомлення" className="text-white px-10 py-2 bg-red-700 font-medium rounded-lg text-xl text-center hover:cursor-pointer"/>
                 </form>
             </div>
             
